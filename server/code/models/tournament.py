68b220b2d76a37b475928b75db34e90dcb1f9d55
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-10 04:39:16 -0500

    Add Default of None to Tournament Surface

--- server/code/models/tournament.py
+++ server/code/models/tournament.py
@@ -4,6 +4,6 @@ class TournamentModel(db.Document):
 
     link = db.URLField()
     name = db.StringField()
-    surface = db.StringField(choices=['Hard', 'Clay', 'Grass', 'Carpet', 'None'])
+    surface = db.StringField(choices=['Hard', 'Clay', 'Grass', 'Carpet', 'None'], default='None')
     gender = db.StringField(min_length=1, max_length=1, default='U')
     sets = db.IntField(max_value=5, default=0)
