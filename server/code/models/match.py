aca1fb68d9201e2b64f3a929e83ef6e548037b88
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-10 05:22:36 -0500

    Add More Columns to Match Model

---
+++ server/code/models/match.py
@@ -0,0 +1,22 @@
+import datetime
+from db import db
+from tournament import TournamentModel
+from player import PlayerModel
+
+class MatchModel(db.Document):
+
+    link = db.URLField(unique=True, required=True)
+    match_date = db.DateTimeField(default=datetime.datetime(1900, 1, 1))
+    gender = db.StringField(min_length=1, max_length=1, default='U')
+    tournament = db.ReferenceField(TournamentModel)
+    match_round = db.StringField(default='NONE')
+    players = db.ListField(db.ReferenceField(PlayerModel))
+    title = db.StringField(default='None')
+    result = db.StringField(default='None')
+    winner = db.ReferenceField(PlayerModel, default='None')
+    loser = db.ReferenceField(PlayerModel, default='None')
+    score = db.StringField(default='None')
+    sets = db.IntField(max_value=5, default=0)
+    points = db.ListField(db.DictField(), default=[])
+
+    meta = {'collection': 'matches'}
