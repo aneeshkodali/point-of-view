0114886b4b39ad4c8b85b4ecfe11046c479011ec
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-10 21:17:42 -0500

    Write Tournament Scraper to Return Tournament Data

--- server/code/scraper/tournament.py
+++ server/code/scraper/tournament.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import ast
+
+from helper import extractVariableFromText
+
 
 def getTournamentData(link):
     '''
@@ -16,12 +20,38 @@ def getTournamentData(link):
     tournament_name = link.split('jstourneys/')[1]
     tournament_dict['gender'] = 'W' if tournament_name.startswith('W_') else 'M'
 
+    # create BeautifulSoup object
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'lxml')
+    soup_text = soup.text
+
     # name
+    try:
+        name = extractVariableFromText(soup_text, 'tname')
+        if name:
+            tournament_dict['name'] = name
+    except:
+        pass
+
     # surface
+    try:
+        surface = soup_text.split('var tsurf=')[1].split(';')[0].replace("'","").replace('"', '')
+        if surface:
+            tournament_dict['surface'] = surface
+    except:
+        pass
+
     # sets
+    # sets is a list-like string: ['All', '2 Sets', '3 Sets', ..., 'RET', 'W/O']
+    # goal is to convert to list, only include elements with 'Sets' in string, then extract the last element integer
+    try:
+        sets_list_str = soup_text.split('var schoices=')[1].split(';')[0]
+        sets_list = ast.literal_eval(sets_list_str)
+        sets_list = [elem.split(' Sets')[0] for elem in sets_list if ' Sets' in elem]
+        sets_num = sets_list[-1]
+        if sets_num:
+            tournament_dict['sets'] = sets_num
+    except:
+        pass
 
     return tournament_dict
-
-tournament_link = 'http://www.minorleaguesplits.com/tennisabstract/cgi-bin/jstourneys/W_2019Us_Open.js'
-tournament_data = getTournamentData(tournament_link)
-print(tournament_data)
