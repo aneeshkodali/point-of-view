539a55cfc17f63610ea7a1281996c023ff63f941
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-14 17:45:03 -0500

    Rewrite Match Resource

--- server/code/resources/match.py
+++ server/code/resources/match.py
@@ -1,5 +1,28 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.match import MatchModel
+from scraper.match import getMatchData
+
+class Match(Resource):
+
+    # create parser
+    parser = reqparse.RequestParser()
+    parser.add_argument('link',
+                        type=str,
+                        required=True,
+                        help='Must supply link'
+                        )
+
+    # POST method
+    def post(self):
+
+        # get link from request
+        data = Match.parser.parse_args()
+        link = data['link']
+
+        # get match_data
+        match_data = getMatchData(link)
+
+        return {'match': match_data}
 
 class Matches(Resource):
 
