3c8fe42686b04a62f5886ff4e5687815560e3666
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-15 10:04:50 -0500

    Show Matches from DB on Matches Page

--- client/src/components/Match/Matches.js
+++ client/src/components/Match/Matches.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import server from '../../api/server';
 
 const Matches = () => {
+
+    // state for matches
+    const [matches, setMatches] = useState([])
+    useEffect(() => {
+        const getMatches = async () => {
+            const response = await server.get('/server/matches');
+            const matches_db = response['data']['matches']
+            setMatches(matches_db)
+        }
+        getMatches()
+    }, []);
+
+
+    const matchesRendered = matches.map(match => {
+        const { _id, title } = match
+        return <li key={_id}>{title}</li>
+    })
+
     return (
         <div>
-            MATCHES PAGE
+            <h1>Matches Page</h1>
+            {matchesRendered}
         </div>
     );
 }
