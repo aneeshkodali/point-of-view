1adf8f478ff267336d3938318c62980f34cb5ba3
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-15 06:42:16 -0500

    Add Player Resource

--- server/code/resources/player.py
+++ server/code/resources/player.py
@@ -1,5 +1,48 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.player import PlayerModel
+from scraper.player import constructPlayerLink, getPlayerData
+
+class Player(Resource):
+
+    # POST method
+    def post(self):
+        # create parser
+        parser = reqparse.RequestParser()
+        parser.add_argument('name',
+                            type=str,
+                            required=True,
+                            help='Must provide player name'
+                            )
+        parser.add_argument('gender',
+                            type=str,
+                            required=True,
+                            help='Must provide player gender'
+                            )
+
+        # get data from parser
+        data = parser.parse_args()
+        name = data['name']
+
+        # if player exists, return 'bad request' error
+        if PlayerModel.find_by_name(name):
+            return {'message': f"Player '{name}' already exists"}, 400
+
+    
+        # create Player object
+        player_link = constructPlayerLink(**data)
+        player_data = getPlayerData(player_link)
+        player_model = PlayerModel(**player_data)
+
+        # insert into db, return 'server' error
+        try:
+            player_model.save()
+        except Exception as e:
+            return {
+                    'message': 'An error occurred inserting the player',
+                    'error': str(e)
+                    }, 500
+        
+        return player_model.json(), 201
 
 class Players(Resource):
 
