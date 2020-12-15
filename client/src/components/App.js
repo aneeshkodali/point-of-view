4caa8e72096e6ec186257d19c329a3f979b3292f
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-15 09:34:37 -0500

    Add Matches Component to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Matches from './Match/Matches';
 
 const App = () => {
     return (
         <div>
-            MY APP
+            <BrowserRouter>
+                HEADER
+                <Switch>
+                    <Route path='/matches' exact component={Matches} />
+                </Switch>
+            </BrowserRouter>
+
+        WELCOME TO MY APP
         </div>
     )
 }
