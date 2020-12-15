90774c1fb0baf15daa9bd83087e4032fca01a8b4
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-15 17:14:21 -0500

    Get Match Data in Match Component

--- client/src/components/Match/Match.js
+++ client/src/components/Match/Match.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import server from '../../api/server';
 
 const Match = (props) => {
-    console.log(props)
+
+    // state for match data
+    const [matchData, setMatchData] = useState({})
+
+    // get match id from props
+    const { id } = props.match.params
+
+    // load match data
+    const getMatchData = async id => {
+        const response = await server.get(`/server/match/${id}`)
+        setMatchData(response.data)
+    }
+    useEffect(() => {
+        getMatchData(id)
+    }, [])
+
+    const { title, result, points } = matchData;
+
+
    return (
        <div>
-           MATCH PAGE
+           <h1 className="ui header">{title}</h1>
+           <h2 className="ui header">{result}</h2>
         </div>
    );
 }
