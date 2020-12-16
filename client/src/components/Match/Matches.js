22a802ddb99a2e7211010883064eead7e5f6fe9f
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-15 20:44:18 -0500

    Add Button to Matches Component

--- client/src/components/Match/Matches.js
+++ client/src/components/Match/Matches.js
@@ -7,25 +7,30 @@ const Matches = () => {
 
     // state for matches
     const [matches, setMatches] = useState([])
-    useEffect(() => {
-        const getMatches = async () => {
-            const response = await server.get('/server/matches');
-            const matches_db = response['data']['matches']
-            setMatches(matches_db)
-        }
-        getMatches()
-    }, []);
+
+    // function to get list of matches
+    const getMatches = async () => {
+        const response = await server.get('/server/matches');
+        const matches_db = response['data']['matches']
+        setMatches(matches_db)
+    }
+
 
 
     const matchesRendered = matches.map(match => {
         const { _id, title } = match
         const id = _id['$oid']
-        return <Link key={id} to={`/matches/${id}`}>{title}</Link>
-    })
+        return (
+            <div key={id}>
+                <Link to={`/matches/${id}`}>{title}</Link>
+            </div>
+        );
+    });
 
     return (
         <div>
             <h1>Matches Page</h1>
+            <button onClick={getMatches}>Get Matches</button>
             {matchesRendered}
         </div>
     );
