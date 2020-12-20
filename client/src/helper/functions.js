62015473cbb65f9f90502184b1d19635900383e5
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-20 17:00:09 -0500

    Add Helper Directory and function file for common functions

---
+++ client/src/helper/functions.js
@@ -0,0 +1,4 @@
+// function to return player data given a player reference
+export const getPlayerData = (player, playerArr) => {
+    return playerArr.filter(p => p['_id']['$oid'] == player['$oid'])[0];
+}
