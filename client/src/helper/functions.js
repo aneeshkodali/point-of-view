7be8e2945069e07cfd4b5ff64be4ce0dc26cd3fd
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-20 17:06:56 -0500

    Fix Equality Comparison in getPlayerData helper function

--- client/src/helper/functions.js
+++ client/src/helper/functions.js
@@ -1,4 +1,4 @@
 // function to return player data given a player reference
 export const getPlayerData = (player, playerArr) => {
-    return playerArr.filter(p => p['_id']['$oid'] == player['$oid'])[0];
+    return playerArr.filter(p => p['_id']['$oid'] === player['$oid'])[0];
 }
