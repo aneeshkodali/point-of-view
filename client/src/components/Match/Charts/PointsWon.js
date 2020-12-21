5bb9b1540a64a8a7b9ebcc5c660db2fa167d8502
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-21 09:23:20 -0500

    Create Initial Points Won Chart

--- client/src/components/Match/Charts/PointsWon.js
+++ client/src/components/Match/Charts/PointsWon.js
@@ -2,15 +2,39 @@ import React from 'react';
 import { VictoryPie } from 'victory';
 
 const PointsWon = ({ matchData }) => {
+
+    const { players, points } = matchData;
+
+    // get pie chart data
+
+    // render charts
+    const chartsRendered = players.map(player => {
+
+        const player_id = player['_id']['$oid'];
+        const point_outcomes = ['ace', 'double fault', 'forced error', 'service winner', 'unforced error', 'winner'];
+        const player_data = [];
+        point_outcomes.forEach(outcome => {
+            const outcome_obj={};
+            outcome_obj['x'] = outcome;
+            outcome_obj['y'] = matchData.points.filter(point => (point['winner']['_id']['$oid'] === player_id) && (point['result'] === outcome)).length
+            player_data.push(outcome_obj);
+        });
+        return (
+            <div className="eight wide column" key={player_id}>
+                <VictoryPie
+                    data={player_data}
+                    colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy', 'pink']}
+                    labels={({datum}) => `${datum.x}:${datum.y}`}
+                    />
+            </div>
+        );
+    })
+
     return (
         <div>
-            <VictoryPie
-                data={[
-                    { x: "Cats", y: 35 },
-                    { x: "Dogs", y: 40 },
-                    { x: "Birds", y: 55 }
-                ]}
-                />
+            <div className="ui grid">
+                {chartsRendered}
+            </div>
         </div>
     );
 }
