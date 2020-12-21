336477d7be6d0c27fc1db6d1a5bafbcc9dc2a7dd
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-21 10:25:13 -0500

    Add Set Filter to Chart

--- client/src/components/Match/Charts/PointsWon.js
+++ client/src/components/Match/Charts/PointsWon.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VictoryPie } from 'victory';
 
 const PointsWon = ({ matchData }) => {
 
-    const { players, points } = matchData;
+    const { players, sets, points } = matchData;
 
-    // get pie chart data
+    // state for set selected
+    const setNums = ['All'].concat(Array.from({length: sets}, (_, i) => i+1));
+    const [setNumSelected, setSetNumSelected] = useState(setNums[0]);
+
+    // create dropdown
+    const setNumOptions = setNums.map(setNum => {
+        return (
+                <option key={setNum} value={setNum}>
+                    {setNum}
+                </option>
+                )
+    });
+
 
     // render charts
     const chartsRendered = players.map(player => {
 
-        const player_id = player['_id']['$oid'];
-        const point_outcomes = ['ace', 'double fault', 'forced error', 'service winner', 'unforced error', 'winner'];
-        const player_data = [];
-        point_outcomes.forEach(outcome => {
-            const outcome_obj={};
-            outcome_obj['x'] = outcome;
-            outcome_obj['y'] = matchData.points.filter(point => (point['winner']['_id']['$oid'] === player_id) && (point['result'] === outcome)).length
-            player_data.push(outcome_obj);
+        const playerID = player['_id']['$oid'];
+        const pointOutcomes = ['ace', 'double fault', 'forced error', 'service winner', 'unforced error', 'winner'];
+        const playerData = [];
+        pointOutcomes.forEach(outcome => {
+            const outcomeObj = {};
+            outcomeObj['x'] = outcome;
+            let pointsFiltered = points.filter(point => (point['winner']['_id']['$oid'] === playerID) && (point['result'] === outcome));
+            //if (setNumSelected !== 'All') {
+            //    pointsFiltered = pointsFiltered.filter(point => point['set_in_match'] === setNumSelected);
+            //}
+            outcomeObj['y'] = pointsFiltered.length;
+            playerData.push(outcomeObj);
         });
         return (
-            <div className="eight wide column" key={player_id}>
+            <div className="eight wide column" key={playerID}>
                 <VictoryPie
-                    data={player_data}
+                    data={playerData}
                     colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy', 'pink']}
                     labels={({datum}) => `${datum.x}:${datum.y}`}
                     />
@@ -32,6 +48,13 @@ const PointsWon = ({ matchData }) => {
 
     return (
         <div>
+            <div>
+                <select 
+                    className="ui dropdown"
+                    value={setNumSelected}
+                    onChange={(e) => setSetNumSelected(e.target.value)}
+                >{setNumOptions}</select>
+            </div>
             <div className="ui grid">
                 {chartsRendered}
             </div>
