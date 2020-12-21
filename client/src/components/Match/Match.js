b10a776e6f2179198c13da32f09f25f13ae16afd
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-21 08:48:51 -0500

    Clean Up Code and Apply getPlayerData Function to Rest of Columns

--- client/src/components/Match/Match.js
+++ client/src/components/Match/Match.js
@@ -36,6 +36,11 @@ const Match = props => {
         const player_two_response = await server.get(`/server/player/${player_two_id}`);
         match_data['players'][1] = player_two_response.data;
 
+        // add data to match winner/loser
+        ['winner', 'loser'].forEach(column => {
+            match_data[column] = getPlayerData(match_data[column], match_data['players']);
+        });
+
         // add player data to points/shots
         match_data['points'] = match_data['points'].map(point => {
             // loop through columns in points
@@ -116,102 +121,5 @@ const Match = props => {
    );
 
 }
-
-//const Match = (props) => {
-
-//    // state for match data
-//    const [matchData, setMatchData] = useState({});
-//    // function to get match data
-//    const getMatchData = async match_id => {
-//        const match_response = await server.get(`/server/match/${match_id}`);
-//        const match_data = match_response.data;
-//        if (match_data) {
-//            setMatchData(match_data);
-//            getTournamentData(match_data['tournament']['$oid']);
-//            getPlayersData(match_data['players']);
-//        }
-//    }
-    
-//    // state for tournament data
-//    const [tournamentData, setTournamentData] = useState({});
-//    // function to get tournament data
-//    const getTournamentData = async tournament_id => {
-//        const tournament_response = await server.get(`/server/tournament/${tournament_id}`);
-//        setTournamentData(tournament_response.data);
-//    }
-
-//    // state for players data
-//    const [playersData, setPlayersData] = useState({});
-//    // function to get players data
-//    const getPlayersData = players => {
-//        const players_data = {};
-//        players.forEach(async player => {
-//            const id = player['$oid'];
-//            const player_response = await server.get(`/server/player/${id}`);
-//            const player_data = player_response.data;
-//            players_data[id] = player_data;
-//        })
-//        setPlayersData(players_data);
-//    }
-
-    
-//    useEffect(() => {
-//        const match_id = props.match.params.id;
-//        getMatchData(match_id);
-//    }, []);
-
-
-//    // state for tabs
-//    const tabs = ['About', 'Data'];
-//    const [tabSelected, setTabSelected] = useState(tabs[0]);
-
-//    // render tabs
-//    const tabsRendered = tabs.map(tab => {
-//        const active = tab === tabSelected ? 'active' : '';
-//        return (
-//            <div key={tab}
-//                className={`${active} item`}
-//                style={{'cursor': 'pointer'}}
-//                onClick={() => setTabSelected(tab)}
-//            >
-//                {tab}
-//            </div>
-//        )
-//    });
-
-//    const data = {
-//        'match': matchData
-//    }
-
-//    // conditionally render tab
-//    const tabComponentRendered = (tab) => {
-//        switch (tab) {
-//            case 'About':
-//                return (
-//                    <About data={data} />  
-//                    //<div>ABOUTA</div>
-//                );
-//            case 'Data':
-//                return (
-//                    //<Table points={matchData.match.points} />
-//                    <div>DATA</div>
-//                );
-//            default:
-//                return null;
-//        }
-//    }
-    
-//    // load component if data is loaded else loader
-//    const componentRendered = (matchData) ? tabComponentRendered(tabSelected) : <Loader text={"Loading Match Data..."} />;
-
-//   return (
-//       <div>
-//           <div className="ui top attached tabular menu">
-//                {tabsRendered}
-//            </div>
-//            {componentRendered}
-//        </div>
-//   );
-//}
 
 export default Match;
