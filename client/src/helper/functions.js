3c77641f00f4652703ae1c89f48fe59d468e4826
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-23 18:23:26 -0500

    Create Helper Function to Convert Date from Epoch to Date String

--- client/src/helper/functions.js
+++ client/src/helper/functions.js
@@ -2,3 +2,19 @@
 export const getPlayerData = (player, playerArr) => {
     return playerArr.filter(p => p['_id']['$oid'] === player['$oid'])[0];
 }
+
+// function to convert date from epoch (in ms) to date string 'yyyy-mm-dd'
+export const convertDate = dateEpoch => {
+    //get date object
+    const dateNew = new Date(0)
+    dateNew.setUTCSeconds(dateEpoch['$date']/1000);
+    // create date string
+    const dateYear = dateNew.getFullYear();
+    const month = dateNew.getMonth()+1;
+    const dateMonth = month < 10 ? `0${month}` : month;
+    const day = dateNew.getDate()+1;
+    const dateDay = day < 10 ? `0${day}` : day;
+    const dateString = `${dateYear}-${dateMonth}-${dateDay}`;
+
+    return dateString;
+}
