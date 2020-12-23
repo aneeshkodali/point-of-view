42015c546eaaefcae440adf994b014608c50c480
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-12-23 18:23:42 -0500

    Use Helper Function to Convert Date

--- client/src/components/Match/About/About.js
+++ client/src/components/Match/About/About.js
@@ -1,28 +1,17 @@
 import React from 'react';
 
+import { convertDate } from '../../../helper/functions';
 import ScoreTable from './ScoreTable';
 
 const About = ({ matchData }) => {
 
     // get variables from match data
     const { title, gender, match_date, match_round, score, tournament, players, winner, loser } = matchData;
-
-    // FORMAT DATE
-    //get date object
-    const match_date_c = new Date(0)
-    match_date_c.setUTCSeconds(match_date['$date']/1000);
-    // create date string
-    const match_date_year = match_date_c.getFullYear();
-    const month = match_date_c.getMonth()+1;
-    const match_date_month = month < 10 ? `0${month}` : month;
-    const day = match_date_c.getDate()+1;
-    const match_date_day = day < 10 ? `0${day}` : day;
-    const match_date_string = `${match_date_year}-${match_date_month}-${match_date_day}`;
    
     return (
         <div>
             <h1 className="ui header">{title}</h1>
-            <div>Date: {match_date_string}</div>
+            <div>Date: {convertDate(match_date)}</div>
             <div>Gender: {gender}</div>
             <div>Tournament: {tournament.name}</div>
             <div>Round: {match_round}</div>
