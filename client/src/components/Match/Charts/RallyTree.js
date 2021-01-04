d28953a36e79e598c67d330e0255e8539f19999d
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-04 07:03:24 -0500

    Use Set Helper Component

--- client/src/components/Match/Charts/RallyTree.js
+++ client/src/components/Match/Charts/RallyTree.js
@@ -2,35 +2,17 @@ import React, { useState } from 'react';
 
 import Side from '../Helper/Side';
 import Player from '../Helper/Player';
+import Set from '../Helper/Set';
 
 const RallyTree = ({ matchData }) => {
 
     const { sets, points, players } = matchData;
 
-    // get variables from Side helper component
+    // get variables from helper components
     const { sideSelected, sideOptions } = Side();
-
-    // get varibles from Player helper component
-    const { playerSelected, playerOptions } = Player(players)
-
-
-    // state for set selected
-    const setNums = ['All'].concat(Array.from({length: sets}, (_, i) => i+1));
-    const [setNumSelected, setSetNumSelected] = useState(setNums[0]);
-
-    // radio options for set selected
-    const setNumOptions = setNums.map(setNum => {
-        const checkedValue = setNum === setNumSelected ? 'checked' : ''
-        return (
-                <div key={setNum} className="field">
-                    <div className="ui radio checkbox">
-                        <input type="radio" name="set" value={setNum} checked={checkedValue} onChange={() => setSetNumSelected(setNum)} />
-                        <label>{setNum}</label>
-                    </div>
-                </div>
-            );
-    });
-
+    const { playerSelected, playerOptions } = Player(players);
+    const { setNumSelected, setNumOptions } = Set(sets);
+    
  
 
     return (
