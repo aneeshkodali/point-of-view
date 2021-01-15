dc6c7e98bbe4ccde81d37a9d957d8a959db6dc3a
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-15 05:49:55 -0500

    Create SurfaceModel (#11)

---
+++ server/code/models/surfaces.py
@@ -0,0 +1,23 @@
+from db import db
+import json
+
+class SurfaceModel(db.Model):
+    '''
+    Court surfaces
+    '''
+
+    surface_id = db.IntField(primary_key=True)
+    surface = db.StringField()
+
+    meta = {'collection': 'surfaces'}
+
+    def json(self):
+        return json.loads(self.to_json())
+
+    @classmethod
+    def find_by_surface_id(cls, surface_id):
+        return SurfaceModel.objects(surface_id=surface_id).first()
+    
+    @classmethod
+    def find_by_surface(cls, surface):
+        return SurfaceModel.objects(surface-surface).first()
