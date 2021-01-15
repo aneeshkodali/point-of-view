bca821750e1de5b7a23a839c929de4fad461c2fe
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-15 07:17:36 -0500

    Allow HandModel to inherit from BaseModel (#11)

--- server/code/models/hands.py
+++ server/code/models/hands.py
@@ -1,23 +1,19 @@
-from db import db
-import json
+from mongoengine import IntField, StringField
+from models.base import BaseModel
 
-class HandModel(db.Document):
+class HandModel(BaseModel):
     '''
-    Hand that the player plays with (right or left)
+    Hand that the player plays with
     '''
 
-    hand_id = db.IntField(primary_key=True)
-    abbreviation = db.StringField()
-    hand = db.StringField()
+    hand_id = IntField(primary_key=True)
+    abbreviation = StringField()
+    hand = StringField()
 
     meta = {'collection': 'hands'}
 
     def json(self):
         return json.loads(self.to_json())
-
-    @classmethod
-    def find_by_hand_id(cls, hand_id):
-        return HandModel.objects(hand_id=hand_id).first()
     
     @classmethod
     def find_by_abbreviation(cls, abbreviation):
