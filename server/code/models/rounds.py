61dedfa26bc891768eaff478f322615d6c1e0ef9
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-15 15:39:45 -0500

    Create RoundModel (#11)

---
+++ server/code/models/rounds.py
@@ -0,0 +1,17 @@
+from mongoengine import IntField, StringField
+from models.base import BaseModel
+
+class RoundModel(BaseModel):
+    '''
+    Match rounds
+    '''
+
+    round_id = IntField(primary_key=True)
+    abbreviation = StringField()
+    round_name = StringField()
+
+    meta = {'collection': 'rounds'}
+
+    @classmethod
+    def find_by_round(cls, round_name):
+        return HandModel.objects(round_name=round_name).first()
