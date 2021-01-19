a0210d6f7254d0e748cfc69a17f4f856e26e5aa6
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-18 21:53:50 -0500

    Comment out code for now (#11)

--- server/code/models/tournament.py
+++ server/code/models/tournament.py
@@ -1,25 +1,25 @@
-from mongoengine.queryset.visitor import Q
-import json
-from db import db
+#from mongoengine.queryset.visitor import Q
+#import json
+#from db import db
 
-class TournamentModel(db.Document):
+#class TournamentModel(db.Document):
 
-    link = db.URLField()
-    name = db.StringField()
-    surface = db.StringField(choices=['Hard', 'Clay', 'Grass', 'Carpet', 'None'], default='None')
-    gender = db.StringField(min_length=1, max_length=1, default='U')
-    sets = db.IntField(max_value=5, default=0)
+#    link = db.URLField()
+#    name = db.StringField()
+#    surface = db.StringField(choices=['Hard', 'Clay', 'Grass', 'Carpet', 'None'], default='None')
+#    gender = db.StringField(min_length=1, max_length=1, default='U')
+#    sets = db.IntField(max_value=5, default=0)
 
-    meta = {'collection': 'tournaments'}
+#    meta = {'collection': 'tournaments'}
 
-    def json(self):
-            return json.loads(self.to_json())
+#    def json(self):
+#            return json.loads(self.to_json())
 
-    @classmethod
-    def find_by_name_and_gender(cls, name, gender):
-        return TournamentModel.objects(Q(name=name) & Q(gender=gender)).first()
+#    @classmethod
+#    def find_by_name_and_gender(cls, name, gender):
+#        return TournamentModel.objects(Q(name=name) & Q(gender=gender)).first()
 
-    @classmethod
-    def find_by_id(cls, id):
-        return TournamentModel.objects(id=id).first()
+#    @classmethod
+#    def find_by_id(cls, id):
+#        return TournamentModel.objects(id=id).first()
 
