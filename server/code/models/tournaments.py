b26f49f584e0522980b1efc7d05337417c4948ef
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-21 06:15:39 -0500

    Change field name to tournament_name_id (#11)

--- server/code/models/tournaments.py
+++ server/code/models/tournaments.py
@@ -10,7 +10,7 @@ class TournamentModel(BaseModel):
     Tournaments - each record is a unique combination of (tournament, gender, year)
     '''
 
-    tournament_id = UUIDField(binary=False, default='4e1f7f4b-6f3e-43ce-954d-aa9cf6ca52e4')
+    tournament_name_id = UUIDField(binary=False, default='4e1f7f4b-6f3e-43ce-954d-aa9cf6ca52e4')
     year = IntField(default=2300)
     gender_id = IntField(default=0)
     date = DateTimeField(default=datetime.datetime(1700, 1, 1))
