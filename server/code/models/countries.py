63666ccf11b204d5d8291d1699586dcb1623fc57
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-24 11:24:12 -0500

    Remove code_three field from CountryModel (#11)

--- server/code/models/countries.py
+++ server/code/models/countries.py
@@ -9,14 +9,9 @@ class CountryModel(BaseModel):
 
     country_id = IntField(primary_key=True)
     country = StringField(unique=True, nullable=False)
-    code_three = StringField(unique=True, nullable=False)
 
     meta = {'collection': 'countries'}
 
     @classmethod
     def find_by_country(cls, country):
         return CountryModel.objects(country=country).first()
-
-    @classmethod
-    def find_by_code_three(cls, code_three):
-        return CountryModel.objects(code_three=code_three).first()
