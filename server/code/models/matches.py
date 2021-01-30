e601d63bfbf5e16193295276aafeadd382494161
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-30 11:38:40 -0500

    Add default tournament_id back into MatchModel for now (#11)

--- server/code/models/matches.py
+++ server/code/models/matches.py
@@ -10,11 +10,10 @@ class MatchModel(BaseModel):
     '''
     Matches
     '''
-#, default='dda2c34f-f992-4701-a76e-e02b12cbdf0e'
     match_id = UUIDField(primary_key=True, default=lambda: uuid4(), binary=False)
     name = StringField(default="")
     date = DateTimeField(default=datetime.datetime(1700, 1, 1))
-    tournament_id = ReferenceField(TournamentModel)
+    tournament_id = ReferenceField(TournamentModel, default='dda2c34f-f992-4701-a76e-e02b12cbdf0e')
     round_id = ReferenceField(RoundModel, default=0)
     score = StringField(default="")
     sets = IntField(default=0)
