e259e85c77f5ad228ae36deac10844e3034654a0
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-30 10:25:20 -0500

    Change tournament scraper function to reference other Object models (#11)

--- server/code/scraper/tournaments.py
+++ server/code/scraper/tournaments.py
@@ -31,12 +31,12 @@ def getTournamentData(link):
         gender = 'W' if tournament_name.startswith('W_') else 'M'
         gender_model_db = GenderModel.find_by_gender(gender)
         if gender_model_db:
-            tournament_model['gender_id'] = gender_model_db.gender_id
+            tournament_model['gender_id'] = gender_model_db
         else:
             gender_id_new = max([gender_model['gender_id'] for gender_model in GenderModel.objects()] or [0]) + 1
             gender_model_new = GenderModel(**{'gender_id': gender_id_new, 'gender': gender})
             gender_model_new.save()
-            tournament_model['gender_id'] = gender_model_new.gender_id
+            tournament_model['gender_id'] = gender_model_new
     except:
         pass
 
@@ -60,11 +60,11 @@ def getTournamentData(link):
         tournament_name = extractVariableFromText(soup_text, 'tname')
         tournament_name_model_db = TournamentNameModel.find_by_name(tournament_name)
         if tournament_name_model_db:
-            tournament_model['tournament_name_id'] = tournament_name_model_db.tournament_name_id
+            tournament_model['tournament_name_id'] = tournament_name_model_db
         else:
             tournament_name_model_new = TournamentNameModel(**{'name': tournament_name})
             tournament_name_model_new.save()
-            tournament_model['tournament_name_id'] = tournament_name_model_new.tournament_name_id
+            tournament_model['tournament_name_id'] = tournament_name_model_new
     except:
         pass
   
@@ -113,12 +113,12 @@ def getTournamentData(link):
         surface = soup_text.split('var tsurf=')[1].split(';')[0].replace("'","").replace('"', '').lower()
         surface_model_db = SurfaceModel.find_by_surface(surface)
         if surface_model_db:
-            tournament_model['surface_id'] = surface_model_db.surface_id
+            tournament_model['surface_id'] = surface_model_db
         else:
             surface_id_new = max([surface_model['surface_id'] for surface_model in SurfaceModel.objects()] or [0]) + 1
             surface_model_new = SurfaceModel(**{'surface_id': surface_id_new, 'surface': surface})
             surface_model_new.save()
-            tournament_model['surface_id'] = surface_model_new.surface_id
+            tournament_model['surface_id'] = surface_model_new
     except:
         pass
    
@@ -127,12 +127,12 @@ def getTournamentData(link):
         level = soup_text.split('var tlev=')[1].split(';')[0].replace("'","").replace('"', '')
         level_model_db = LevelModel.find_by_level(level)
         if level_model_db:
-            tournament_model['level_id'] = level_model_db.level_id
+            tournament_model['level_id'] = level_model_db
         else:
             level_id_new = max([level_model['level_id'] for level_model in LevelModel.objects()] or [0]) + 1
             level_model_new = LevelModel(**{'level_id': level_id_new, 'level': level})
             level_model_new.save()
-            tournament_model['level_id'] = level_model_new.level_id
+            tournament_model['level_id'] = level_model_new
     except:
         pass
 
