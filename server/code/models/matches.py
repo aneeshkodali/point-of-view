1aae1a6450c7058a9470985dc89926c7c74a2869
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-01-30 10:30:20 -0500

    Add ReferenceFields to MatchModel (#11)

--- server/code/models/matches.py
+++ server/code/models/matches.py
@@ -1,8 +1,10 @@
-from mongoengine import UUIDField, URLField, DateTimeField, IntField, StringField
+from mongoengine import UUIDField, URLField, DateTimeField, IntField, StringField, ReferenceField
 import datetime
 from uuid import uuid4
 
 from models.base import BaseModel
+from models.rounds import RoundModel
+from models.tournaments import TournamentModel
 
 class MatchModel(BaseModel):
     '''
@@ -12,8 +14,8 @@ class MatchModel(BaseModel):
     match_id = UUIDField(primary_key=True, default=lambda: uuid4(), binary=False)
     name = StringField(default="")
     date = DateTimeField(default=datetime.datetime(1700, 1, 1))
-    tournament_id = UUIDField(binary=False, default='dda2c34f-f992-4701-a76e-e02b12cbdf0e')
-    round_id = IntField(default=0)
+    tournament_id = ReferenceField(TournamentModel, default='dda2c34f-f992-4701-a76e-e02b12cbdf0e')
+    round_id = ReferenceField(RoundModel, default=0)
     score = StringField(default="")
     sets = IntField(default=0)
     link = URLField(unique=True, nullable=False)
