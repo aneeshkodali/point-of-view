a6a6ff2bedf134689f1b99664dfcf34b67ab275b
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-06 22:25:27 -0500

    Remove reference to db object (#11)

--- server/code/models/base.py
+++ server/code/models/base.py
@@ -1,7 +1,6 @@
-from mongoengine import StringField
+import mongoengine as db
 import json
 
-from db import db
 from models.default_values import default_uuid_value
 
 class BaseModel(db.Document):
@@ -9,7 +8,7 @@ class BaseModel(db.Document):
     Base model used to build other models
     '''
 
-    #uuid = StringField(unique=True, nullable=False, default=default_uuid_value)
+    #uuid = db.StringField(unique=True, nullable=False, default=default_uuid_value)
 
     # allows for 'inheritance' while allowing other models to be created as separate collections
     meta = {'abstract': True}
