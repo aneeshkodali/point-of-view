25858ecf71940df9ce0e5a13504e1103f382b459
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-09 07:16:13 -0500

    Create SetModel (#11)

---
+++ server/code/models/sets.py
@@ -0,0 +1,30 @@
+from mongoengine import IntField, StringField, ReferenceField
+from mongoengine.queryset.visitor import Q
+
+from models.base import BaseModel
+from models.default_values import default_uuid_value, default_date_value
+from models.matches import MatchModel
+
+class SetModel(BaseModel):
+    '''
+    Sets
+    '''
+
+    set_id = StringField(primary_key=True, default=default_uuid_value)
+    match_id = ReferenceField(MatchModel)
+    set_in_match = IntField(default=0)
+    score = StringField(default="")
+
+    meta = {'collection': 'sets'}
+
+    def json(self):
+        return {
+            'set_id': self.set_id,
+            'match_id': self.match_id,
+            'set_in_match': self.set_in_match,
+            'score': self.score
+        }
+
+    @classmethod
+    def find_by_set_id_and_match_id_and_player_id(cls, set_id, match_id):
+        return MatchPlayerModel.objects(Q(set_id=set_id)& Q(match_id=match_id)).first()
