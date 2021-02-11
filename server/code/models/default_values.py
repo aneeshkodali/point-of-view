2af900ded0f0662f0c2fb9a657a9e3bc3acf56d2
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-11 07:47:23 -0500

    Remove hyphens from uuid value (#11)

--- server/code/models/default_values.py
+++ server/code/models/default_values.py
@@ -1,9 +1,9 @@
 from uuid import uuid4
 import datetime
 
-default_uuid_value = lambda: str(uuid4())
-default_tournament_id_value = 'dda2c34f-f992-4701-a76e-e02b12cbdf0e'
-default_tournament_name_id_value = '4e1f7f4b-6f3e-43ce-954d-aa9cf6ca52e4'
+default_uuid_value = lambda: str(uuid4()).replace('-','')
+default_tournament_id_value = 'dda2c34f-f992-4701-a76e-e02b12cbdf0e'.replace('-','')
+default_tournament_name_id_value = '4e1f7f4b-6f3e-43ce-954d-aa9cf6ca52e4'.replace('-','')
 
 default_year_value = 1700
 default_month_value = 1
