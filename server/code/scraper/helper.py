4e9d7cc456e319f410b9c78fadafa1aa1e4402e5
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-12 16:43:35 -0500

    Add function to retrieve or create GenderModel (#11)

--- server/code/scraper/helper.py
+++ server/code/scraper/helper.py
@@ -1,3 +1,22 @@
+from models.genders import GenderModel
+
+def getGenderModel(gender):
+    '''
+    Takes a gender and queries GenderModel for record
+    Return record or create new one if not found
+    '''
+
+    gender_model_db = GenderModel.objects(gender=gender).first()
+    if gender_model_db:
+        return gender_model_db
+
+    gender_id_new = max([gender_model['gender_id'] for gender_model in GenderModel.objects()] or [0]) + 1
+    gender_model_new = GenderModel(**{'gender_id': gender_id_new, 'gender': gender})
+    gender_model_new.save()
+
+    return gender_model_new
+
+
 def extractVariableFromText(text, variable):
     '''
     Takes in a blob of text and splits accordingly to return a text value
