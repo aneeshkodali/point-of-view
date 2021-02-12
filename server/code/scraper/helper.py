c6f52c21dddfc5259301810c1c9bfbf3db5e1cb6
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-12 16:54:23 -0500

    Add function to retrieve or create HandModel (#11)

--- server/code/scraper/helper.py
+++ server/code/scraper/helper.py
@@ -1,4 +1,5 @@
 from models.genders import GenderModel
+from models.hands import HandModel
 
 def getGenderModel(gender):
     '''
@@ -15,6 +16,23 @@ def getGenderModel(gender):
     gender_model_new.save()
 
     return gender_model_new
+
+
+def getHandModel(hand):
+    '''
+    Takes a hand and queries HandModel for record
+    Return record or create new one if not found
+    '''
+
+    hand_model_db = HandModel.objects(hand=hand).first()
+    if hand_model_db:
+        return hand_model_db
+
+    hand_id_new = max([hand_model['hand_id'] for hand_model in HandModel.objects()] or [0]) + 1
+    hand_model_new = HandModel(**{'hand_id': hand_id_new, 'hand': hand})
+    hand_model_new.save()
+
+    return hand_model_new
 
 
 def extractVariableFromText(text, variable):
