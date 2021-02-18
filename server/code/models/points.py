97a733c469c49f74b5ffb11135f68ce4c315edcd
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-18 08:23:10 -0500

    Create PointModel (#11)

---
+++ server/code/models/points.py
@@ -0,0 +1,33 @@
+from mongoengine import IntField, StringField, ReferenceField
+
+from models.base import BaseModel
+from models.default_values import default_uuid_value
+from models.games import GameModel
+from mdoels.sides import SideModel
+
+class PointModel(BaseModel):
+    '''
+    Points
+    '''
+
+    point_id = StringField(primary_key=True, default=default_uuid_value)
+    game = ReferenceField(GameModel)
+    point_in_game = IntField(default=0)
+    point_in_set = IntField(default=0)
+    point_in_match = IntField(default=0)
+    side = ReferenceField(SideModel)
+    score = StringField()
+
+    meta = {'collection': 'points'}
+
+    def json(self):
+        return {
+            'point_id': self.point_id,
+            'game': self.game,
+            'point_in_game': self.point_in_game,
+            'point_in_set': self.point_in_set,
+            'point_in_match': self.point_in_match,
+            'side': self.side,
+            'score': self.score
+        }
+
