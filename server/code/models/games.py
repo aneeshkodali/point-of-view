d847778e77177e3fe37129c98fedce1f46181df2
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-18 07:35:36 -0500

    Remove server from GameModel (will add to tb GamePlayerModel (#11)

--- server/code/models/games.py
+++ server/code/models/games.py
@@ -2,7 +2,6 @@ from mongoengine import IntField, StringField, ReferenceField
 
 from models.base import BaseModel
 from models.default_values import default_uuid_value
-from models.player import PlayerModel
 from models.sets import SetModel
 
 class GameModel(BaseModel):
@@ -11,7 +10,6 @@ class GameModel(BaseModel):
     '''
 
     game_id = StringField(primary_key=True, default=default_uuid_value)
-    server = ReferenceField(PlayerModel)
     match_set = ReferenceField(SetModel)
     game_in_set = IntField(default=0)
     game_in_match = IntField(default=0)
@@ -22,7 +20,6 @@ class GameModel(BaseModel):
     def json(self):
         return {
             'game_id': self.game_id,
-            'server': self.server,
             'match_set': self.match_set,
             'game_in_set': self.game_in_set,
             'game_in_match': self.game_in_match,
