c415c9510d391a8a159b5f029e1c84efc195a0cc
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-03-05 10:57:52 -0500

    Create ShotModel (#11)

---
+++ server/code/models/shots.py
@@ -0,0 +1,20 @@
+from mongoengine import IntField, StringField, ReferenceField
+
+from models.base import BaseModel
+from models.default_values import default_uuid_value
+from models.players import PlayerModel
+from models.points import PointModel
+
+class ShotModel(BaseModel):
+    '''
+    Shots
+    '''
+
+    shot_id = StringField(primary_key=True, default=default_uuid_value)
+    point = ReferenceField(PointModel)
+    shot_number = IntField()
+    shot_number_w_serve = IntField()
+    shot_by = db.ReferenceField(PlayerModel)
+    shot = db.StringField()
+    location = db.StringField()
+    result = db.StringField()
